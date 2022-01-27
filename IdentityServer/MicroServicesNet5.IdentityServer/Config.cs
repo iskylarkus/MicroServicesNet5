d2543f35401fdfc7d4030738a052c18529c749ba
iskylarkus <turgay.kibar@gmail.com>
2022-01-28 00:33:31 +0300

    controller developed

--- IdentityServer/MicroServicesNet5.IdentityServer/Config.cs
+++ IdentityServer/MicroServicesNet5.IdentityServer/Config.cs
@@ -16,6 +16,7 @@ namespace MicroServicesNet5.IdentityServer
             new ApiResource("resource_catalog"){ Scopes = { "catalog_fullpermission"}},
             new ApiResource("resource_photo_stock"){ Scopes = { "photo_stock_fullpermission"}},
             new ApiResource("resource_basket"){ Scopes = { "basket_fullpermission"}},
+            new ApiResource("resource_discount"){ Scopes = { "discount_fullpermission"}},
             new ApiResource(IdentityServerConstants.LocalApi.ScopeName)
         };
 
@@ -34,6 +35,7 @@ namespace MicroServicesNet5.IdentityServer
                 new ApiScope("catalog_fullpermission", "Catalog API için full erişim"),
                 new ApiScope("photo_stock_fullpermission", "Photo Stock API için full erişim"),
                 new ApiScope("basket_fullpermission", "Basket API için full erişim"),
+                new ApiScope("discount_fullpermission", "Discount API için full erişim"),
                 new ApiScope(IdentityServerConstants.LocalApi.ScopeName)
             };
 
@@ -58,6 +60,7 @@ namespace MicroServicesNet5.IdentityServer
                     AllowedScopes =
                     {
                         "basket_fullpermission",
+                        "discount_fullpermission",
                         IdentityServerConstants.StandardScopes.Email,
                         IdentityServerConstants.StandardScopes.OpenId,
                         IdentityServerConstants.StandardScopes.Profile,
---
+++ Services/Discount/MicroServicesNet5.Services.Discount/Controllers/DiscountsController.cs
@@ -0,0 +1,63 @@
+﻿using MicroServicesNet5.Services.Discount.Models;
+using MicroServicesNet5.Services.Discount.Services;
+using MicroServicesNet5.Shared.Controller;
+using MicroServicesNet5.Shared.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace MicroServicesNet5.Services.Discount.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DiscountsController : CustomBaseController
+    {
+        private readonly IDiscountService _discountService;
+        private readonly ISharedIdentityService _sharedIdentityService;
+
+        public DiscountsController(IDiscountService discountService, ISharedIdentityService sharedIdentityService)
+        {
+            _discountService = discountService;
+            _sharedIdentityService = sharedIdentityService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            return CreateActionResultInstance(await _discountService.GetAll());
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            return CreateActionResultInstance(await _discountService.GetById(id));
+        }
+
+        [HttpGet]
+        //[Route("/api/[controller]/[action]/{code}/{userId}")]
+        //[Route("/api/[controller]/GetByCode/{code}")]
+        [Route("/api/[controller]/[action]/{code}")]
+        public async Task<IActionResult> GetByCode(string code)
+        {
+            return CreateActionResultInstance(await _discountService.GetByCodeAndUserId(code, _sharedIdentityService.GetUserId));
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Save(DiscountModel discountModel)
+        {
+            return CreateActionResultInstance(await _discountService.Save(discountModel));
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Update(DiscountModel discountModel)
+        {
+            return CreateActionResultInstance(await _discountService.Update(discountModel));
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            return CreateActionResultInstance(await _discountService.Delete(id));
+        }
+    }
+}
