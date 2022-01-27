ec95e3521242cfcbb70a1f11818b5838b3c4a1c4
iskylarkus <turgay.kibar@gmail.com>
2022-01-28 01:01:27 +0300

    discount api project finished

--- discount.sql
+++ discount.sql
@@ -1,7 +1,17 @@
+CREATE TABLE public.discount (
+	id serial4 NOT NULL,
+	userid varchar(200) NOT NULL,
+	rate int2 NOT NULL,
+	code varchar(50) NOT NULL,
+	createddate timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	CONSTRAINT discount_pkey PRIMARY KEY (id)
+);
+/*
 create table discount (
 	Id serial primary key,
 	UserId varchar(200) unique not null,
 	Rate smallint not null,
 	Code varchar(50) not null,
 	CreatedDate timestamp not null default CURRENT_TIMESTAMP
-)
+);
+*/
