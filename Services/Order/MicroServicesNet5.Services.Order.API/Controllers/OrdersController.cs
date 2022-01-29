d3d6bab4187bd5136f8f62e1f93cbaab40d81d19
iskylarkus <turgay.kibar@gmail.com>
2022-01-30 00:05:02 +0300

    orders controller developed

---
+++ Services/Order/MicroServicesNet5.Services.Order.API/Controllers/OrdersController.cs
@@ -0,0 +1,41 @@
+﻿using MediatR;
+using MicroServicesNet5.Services.Order.Application.Commands;
+using MicroServicesNet5.Services.Order.Application.Queries;
+using MicroServicesNet5.Shared.Controller;
+using MicroServicesNet5.Shared.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace MicroServicesNet5.Services.Order.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrdersController : CustomBaseController
+    {
+        private readonly IMediator _mediator;
+        private readonly ISharedIdentityService _sharedIdentityService;
+
+        public OrdersController(IMediator mediator, ISharedIdentityService sharedIdentityService)
+        {
+            _mediator = mediator;
+            _sharedIdentityService = sharedIdentityService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetOrders()
+        {
+            var response = await _mediator.Send(new GetOrdersByUserIdQuery { UserId = _sharedIdentityService.GetUserId });
+
+            return CreateActionResultInstance(response);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SaveOrders(CreateOrderCommand createOrderCommand)
+        {
+            var response = await _mediator.Send(createOrderCommand);
+            
+            return CreateActionResultInstance(response);
+        }
+    }
+}
--- Services/Order/MicroServicesNet5.Services.Order.API/Startup.cs
+++ Services/Order/MicroServicesNet5.Services.Order.API/Startup.cs
@@ -1,7 +1,12 @@
+using MediatR;
 using MicroServicesNet5.Services.Order.Infrastructure;
+using MicroServicesNet5.Shared.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Authorization;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -10,6 +15,7 @@ using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -27,6 +33,17 @@ namespace MicroServicesNet5.Services.Order.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            var requireAuthorizePolicy = new AuthorizationPolicyBuilder().RequireAuthenticatedUser().Build();
+
+            JwtSecurityTokenHandler.DefaultInboundClaimTypeMap.Remove("sub");
+
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
+            {
+                options.Authority = Configuration["IdentityServerURL"];
+                options.Audience = "resource_order";
+                options.RequireHttpsMetadata = false;
+            });
+
             services.AddDbContext<OrderDbContext>(opt =>
             {
                 opt.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"), conf =>
@@ -35,7 +52,18 @@ namespace MicroServicesNet5.Services.Order.API
                 });
             });
 
-            services.AddControllers();
+
+            services.AddHttpContextAccessor();
+
+            services.AddScoped<ISharedIdentityService, SharedIdentityService>();
+
+            services.AddMediatR(typeof(MicroServicesNet5.Services.Order.Application.Handlers.CreateOrderCommandHandler).Assembly);
+
+
+            services.AddControllers(opt =>
+            {
+                opt.Filters.Add(new AuthorizeFilter(requireAuthorizePolicy));
+            });
 
             services.AddSwaggerGen(c =>
             {
@@ -54,6 +82,8 @@ namespace MicroServicesNet5.Services.Order.API
             }
 
             app.UseRouting();
+
+            app.UseAuthentication();
 
             app.UseAuthorization();
 
