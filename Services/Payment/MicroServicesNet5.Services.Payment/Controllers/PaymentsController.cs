bb045db943d5f0b8e26bab944b6364b6847bdd82
iskylarkus <turgay.kibar@gmail.com>
2022-01-30 00:42:41 +0300

    payments controller developed

---
+++ Services/Payment/MicroServicesNet5.Services.Payment/Controllers/PaymentsController.cs
@@ -0,0 +1,18 @@
+﻿using MicroServicesNet5.Shared.Controller;
+using MicroServicesNet5.Shared.Dtos;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MicroServicesNet5.Services.Payment.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PaymentsController : CustomBaseController
+    {
+        [HttpPost]
+        public IActionResult ReceivePayment()
+        {
+            return CreateActionResultInstance(Response<NoContent>.Success(200));
+        }
+    }
+}
