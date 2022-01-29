3f93e908d282dac7a70dec3429eae6cf2b23e300
iskylarkus <turgay.kibar@gmail.com>
2022-01-30 02:36:14 +0300

    configuration development json finished

--- IdentityServer/MicroServicesNet5.IdentityServer/Config.cs
+++ IdentityServer/MicroServicesNet5.IdentityServer/Config.cs
@@ -19,6 +19,7 @@ namespace MicroServicesNet5.IdentityServer
             new ApiResource("resource_discount"){ Scopes = { "discount_fullpermission"}},
             new ApiResource("resource_order"){ Scopes = { "order_fullpermission"}},
             new ApiResource("resource_payment"){ Scopes = { "payment_fullpermission"}},
+            new ApiResource("resource_gateway"){ Scopes = { "gateway_fullpermission"}},
             new ApiResource(IdentityServerConstants.LocalApi.ScopeName)
         };
 
@@ -40,6 +41,7 @@ namespace MicroServicesNet5.IdentityServer
                 new ApiScope("discount_fullpermission", "Discount API için full erişim"),
                 new ApiScope("order_fullpermission", "Order API için full erişim"),
                 new ApiScope("payment_fullpermission", "Payment API için full erişim"),
+                new ApiScope("gateway_fullpermission", "Gateway API için full erişim"),
                 new ApiScope(IdentityServerConstants.LocalApi.ScopeName)
             };
 
@@ -52,7 +54,13 @@ namespace MicroServicesNet5.IdentityServer
                     ClientId = "WebMvcClient",
                     ClientSecrets = {new Secret("secret".Sha256())},
                     AllowedGrantTypes = GrantTypes.ClientCredentials,
-                    AllowedScopes = { "catalog_fullpermission", "photo_stock_fullpermission", IdentityServerConstants.LocalApi.ScopeName }
+                    AllowedScopes = 
+                    {
+                        "catalog_fullpermission",
+                        "photo_stock_fullpermission",
+                        "gateway_fullpermission",
+                        IdentityServerConstants.LocalApi.ScopeName
+                    }
                 },
                 new Client
                 {
@@ -67,6 +75,7 @@ namespace MicroServicesNet5.IdentityServer
                         "discount_fullpermission",
                         "order_fullpermission",
                         "payment_fullpermission",
+                        "gateway_fullpermission",
                         IdentityServerConstants.StandardScopes.Email,
                         IdentityServerConstants.StandardScopes.OpenId,
                         IdentityServerConstants.StandardScopes.Profile,
